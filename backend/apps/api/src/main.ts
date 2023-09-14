57b72b78e3be0b5b6acdfb0b95f05d902d2305cd
Humberto Piaia <betopiaia@gmail.com>
2023-09-14 01:05:36 -0300

    setup api as microservice

--- backend/apps/api/src/main.ts
+++ backend/apps/api/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core'
 import { ApiModule } from './api.module'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiModule)
+  await app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.REDIS,
+    options: {
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT),
+    },
+  })
+
   await app.listen(3001)
+
 }
 
 bootstrap()
