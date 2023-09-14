082885f177c1f2bb917c1a7d3b0311bc843e2502
Humberto Piaia <betopiaia@gmail.com>
2023-09-14 00:32:34 -0300

    install prettier and create backend project

---
+++ backend/apps/worker/src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core'
+import { WorkerModule } from './worker.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(WorkerModule)
+  await app.listen(3002)
+}
+
+bootstrap()
