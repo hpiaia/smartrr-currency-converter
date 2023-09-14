ca1a46f13437116fbb6ab44603516bff2461a9d9
Humberto Piaia <betopiaia@gmail.com>
2023-09-14 01:02:26 -0300

    setup worker as microservice

--- backend/apps/worker/src/main.ts
+++ backend/apps/worker/src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core'
+import { MicroserviceOptions, Transport } from '@nestjs/microservices'
 import { WorkerModule } from './worker.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(WorkerModule)
-  await app.listen(3002)
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(WorkerModule, {
+    transport: Transport.REDIS,
+    options: {
+      host: process.env.REDIS_HOST,
+      port: parseInt(process.env.REDIS_PORT),
+    },
+  })
+
+  await app.listen()
 }
 
 bootstrap()
